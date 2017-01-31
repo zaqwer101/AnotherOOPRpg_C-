a10a90752259038ced2cf47c18a05ed4ffdb68e4
zaqwer101 <platonov256@gmail.com>
2017-02-01 04:52:03 +0800

    Добавлена форма для всех перков(пустая блеа)

--- AnotherOOPGame/AnotherOOPGame/Forms/AllPerks.cs
+++ AnotherOOPGame/AnotherOOPGame/Forms/AllPerks.cs
@@ -1,65 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
-
-namespace AnotherOOPGame.Forms
+namespace AnotherOOPGame
 {
-
-	public partial class HeroPerks : Form
+	public partial class AllPerks : Form
 	{
+		Creature hero;
+		List<Perk> perks;
 
-		public Creature hero;
-		List<EPerk> perks;
-		public HeroPerks(Creature hero)
+		public AllPerks(Creature hero)
 		{
-
+			perks = new List<Perk>
+			{
+			new Perks.FireBall(hero), new Perks.BaseHeal(hero)
+			};
 			this.hero = hero;
-			InitializeComponent();
+			Panel panel1 = new Panel();
+			Controls.Add(panel1);
+			//Дальше на панель накидать перков, которые есть в списке, но нет у существа
 		}
+	}
 
-		private void Perks_Load(object sender, EventArgs e)
+	class EPerk //Вспомогательный класс
+	{
+		public Label name;
+		public Button buy;
+
+		public EPerk(Label name, Button buy)
 		{
-
-			perks = new List<EPerk>();
-
-			this.Controls.Add(panel1);
-
-			for (int i = 0; i < hero.perks.Count; i++)
-			{
-				perks.Add(
-					new EPerk(
-						new Label() { Text = hero.perks[i].name, Location = new Point(0, i * 50) },
-						new Label() { Text = hero.perks[i].lvl + "", Location = new Point(150, i * 50) },
-						new Button() { Text = "+", Location = new Point(300, i * 50), Name = "button_upgrade" + i }
-						)
-					);
-
-				panel1.Controls.Add(perks[perks.Count - 1].name);
-				panel1.Controls.Add(perks[perks.Count - 1].value);
-				panel1.Controls.Add(perks[perks.Count - 1].upgrade);
-
-				panel1.Update();
-				Update();
-				perks[perks.Count - 1].upgrade.Click += new EventHandler(delegate (object s, EventArgs args)
-				{
-					MessageBox.Show(hero.upgradePerk(i));
-					this.Update();
-				});
-			}
-		}
-
-		class EPerk //Вспомогательный класс
-		{
-			public Label name, value;
-			public Button upgrade;
-
-			public EPerk(Label name, Label value, Button upgrade)
-			{
-				this.name = name;
-				this.value = value;
-				this.upgrade = upgrade;
-			}
+			this.name = name;
+			this.buy = buy;
 		}
 	}
 }
